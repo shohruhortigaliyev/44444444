31f006d1bdab6ac873be0dfca7c89c0247d0a1d6
Shoxruh <ortigaliyevshohruh@gmail.com>
2026-06-22 12:27:11 +0500

    first commit

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { auth, requireAdmin } = require("../middleware/auth");
-const ctrl = require("../controllers/users");
-
-router.get("/", auth, requireAdmin, ctrl.getUsers);
-router.delete("/:id", auth, requireAdmin, ctrl.deleteUser);
-
-module.exports = router;
